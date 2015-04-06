4fd9d1ba61c1d3c3146f5d3d8da1ec890a482f69
Justin Jacobs <jajdorkster@gmail.com>
2015-04-06 11:33:52 -0400

    CPU/Memory/Network patch
    Original author: Fran

--- src/panel.c
+++ src/panel.c
@@ -146,6 +146,7 @@ void init_panel()
 	init_systray();
 	init_launcher();
 	init_clock();
+	init_graphs();
 #ifdef ENABLE_BATTERY
 	init_battery();
 #endif
@@ -183,6 +184,8 @@ void init_panel()
 				init_launcher_panel(p);
 			if (panel_items_order[k] == 'T')
 				init_taskbar_panel(p);
+			if (panel_items_order[k] == 'G')
+				init_graphs_panel(p);
 #ifdef ENABLE_BATTERY
 			if (panel_items_order[k] == 'B')
 				init_battery_panel(p);
@@ -484,6 +487,8 @@ void set_panel_items_order(Panel *p)
 			for (j=0 ; j < p->nb_desktop ; j++)
 				p->area.list = g_slist_append(p->area.list, &p->taskbar[j]);
 		}
+		if (panel_items_order[k] == 'G')
+			p->area.list = g_slist_append(p->area.list, &p->graphs);
 #ifdef ENABLE_BATTERY
 		if (panel_items_order[k] == 'B') 
 			p->area.list = g_slist_append(p->area.list, &p->battery);
@@ -761,6 +766,20 @@ int click_padding(Panel *panel, int x, int y)
 		return 1;
 	}
 	return 0;
+}
+
+
+int click_graphs(Panel *panel, int x, int y)
+{
+	Graphs gr = panel->graphs;
+	if (panel_horizontal) {
+		if (gr.area.on_screen && x >= gr.area.posx && x <= (gr.area.posx + gr.area.width))
+			return TRUE;
+	} else {
+		if (gr.area.on_screen && y >= gr.area.posy && y <= (gr.area.posy + gr.area.height))
+			return TRUE;
+	}
+	return FALSE;
 }
 
 
